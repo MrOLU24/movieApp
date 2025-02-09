e23dbe647e56a51bd5fad7ac00e7596dd86a02ef
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-02-09 21:27:54 +0100

    year and lang

--- src/Components/Moviecard.jsx
+++ src/Components/Moviecard.jsx
@@ -11,8 +11,18 @@ const MovieCard = ({
             ? `https://image.tmdb.org/t/p/w500/${poster_path}`
             : "/no-image"
         }
-        alt=""
+        alt={title}
       />
+      <div className="mt-4">
+        <h3>{title}</h3>
+        <div className="content">
+          <div className="rating">
+            <img src="star.svg" alt="star-icon" />
+            <p>{vote_average ? vote_average.toFixed(1) : "N/A"}</p>
+          </div>
+          <span>•</span>
+        </div>
+      </div>
     </div>
   );
 };
