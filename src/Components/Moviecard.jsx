7b5defec8118e993de82b142437b25b9f462403d
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-02-09 21:15:04 +0100

    movie image

--- src/Components/Moviecard.jsx
+++ src/Components/Moviecard.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from "react";
 
-const MovieCard = ({ movie }) => {
-    return (
-        <div>
-            <h1>hello</h1>
-        </div>
-    );
+const MovieCard = ({
+  movie: { title, vote_average, poster_path, release_date, original_language },
+}) => {
+  return (
+    <div className="movie-card">
+      <img
+        src={
+          poster_path
+            ? `https://image.tmdb.org/t/p/w500/${poster_path}`
+            : "/no-image"
+        }
+        alt=""
+      />
+    </div>
+  );
 };
 
 export default MovieCard;
